757aa08dbf9a0ce55b1cf433f67b13214e219e92
Vitor Hugo <vitorhugo.pitolsilva@gmail.com>
2020-08-27 19:50:27 -0400

    Add eslint and prettier config

--- backend/start/kernel.js
+++ backend/start/kernel.js
@@ -1,7 +1,5 @@
-'use strict'
-
 /** @type {import('@adonisjs/framework/src/Server')} */
-const Server = use('Server')
+const Server = use('Server');
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +16,7 @@ const globalMiddleware = [
   'Adonis/Middleware/Shield',
   'Adonis/Middleware/AuthInit',
   'App/Middleware/ConvertEmptyStringsToNull',
-]
+];
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +37,8 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
-}
+  guest: 'Adonis/Middleware/AllowGuestOnly',
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -52,12 +50,8 @@ const namedMiddleware = {
 | control over request lifecycle.
 |
 */
-const serverMiddleware = [
-  'Adonis/Middleware/Static',
-  'Adonis/Middleware/Cors'
-]
+const serverMiddleware = ['Adonis/Middleware/Static', 'Adonis/Middleware/Cors'];
 
-Server
-  .registerGlobal(globalMiddleware)
+Server.registerGlobal(globalMiddleware)
   .registerNamed(namedMiddleware)
-  .use(serverMiddleware)
+  .use(serverMiddleware);
