49841ef70bf50ea6ffe8aed70c7e324f0cf1ccb2
hugokishi <hugopacheco370@gmail.com>
2020-08-27 20:59:12 -0300

    docker-compose mysql config

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -50,10 +50,10 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
+      host: Env.get('DB_HOST', '127.0.0.1'),
+      port: Env.get('DB_PORT', '3306'),
       user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
+      password: Env.get('DB_PASSWORD', 'root'),
       database: Env.get('DB_DATABASE', 'adonis')
     }
   },
